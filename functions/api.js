b0f9cd6d0e5645a53c173894f59582443d84d5e0
unknown <ibraheemhaseeb7@gmail.com>
2023-04-25 21:31:21 +0500

    some changes again, I honestly dont remember what I did :)

--- functions/api.js
+++ functions/api.js
@@ -2,27 +2,22 @@
 const express = require("express");
 const cors = require("cors");
 const serverless = require("serverless-http");
-require("dotenv").config();
-const PORT = process.env.PORT;
+// require("dotenv").config();
+// const PORT = process.env.PORT;
 
 // INITIALIZING THE EXPRESS BASIC FUNCTIONS
 const app = express();
 const router = express.Router();
 
 // DEFINING SOME RULES FOR THE EXPRESS
-app.use(express.json());
-app.use(router);
+// app.use(express.json());
+// app.use(router);
 app.use(cors());
-app.use("./netlify/functions/api", router);
+app.use("/.netlify/functions/api", router);
 
 // API ROUTES
-router.get("/", (req, res, next) => {
+router.get("/demo", (req, res, next) => {
   res.status(200).json({ response: "hello world" });
-});
-
-// STARTING THE SERVER
-app.listen(PORT, () => {
-  console.log(`Local host running on ${PORT}`);
 });
 
 // EXPRORTING AS A SERVERLESS FUNCTION
